3fb615e5774d189da78e32afc338be271dab58e5
barcek <barcek@gmx.com>
2022-11-05 20:03:39 +0000

    feature: extended for access via the command line & added default text & show option for method display

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+/*
+  Imports
+*/
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+import { run } from './lib/index.js';
+
+/*
+  CLI use
+*/
+
+const pathHere = fileURLToPath(import.meta.url);
+const pathUsed = process.argv[1];
+
+if( dirname(pathHere) === pathUsed || pathHere === pathUsed ) run(pathHere, process.argv);
+
+/*
+  Exports
+*/
+
 export { default } from './lib/index.js';
